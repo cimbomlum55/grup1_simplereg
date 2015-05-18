1c92cb92b83fc90390f66711d381e8105f8a6a8c
Administrator <Administrator@10.10.10.132>
2015-05-18 11:09:00 +0300

    ilk versiyon

---
+++ src/main/java/tr/gov/ptt/gr1_simplereq/dto/User.java
@@ -0,0 +1,91 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tr.gov.ptt.gr1_simplereq.dto;
+
+/**
+ *
+ * @author Administrator
+ */
+public class User {
+    
+    private String ad;
+    private String soyad;
+    private Integer id;
+    private String kullaniciAdi;
+    private String parola;    
+    private String email;
+    private String cepTel;
+    private Integer yas;
+
+    public String getAd() {
+        return ad;
+    }
+
+    public void setAd(String ad) {
+        this.ad = ad;
+    }
+
+    public String getSoyad() {
+        return soyad;
+    }
+
+    public void setSoyad(String soyad) {
+        this.soyad = soyad;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getKullaniciAdi() {
+        return kullaniciAdi;
+    }
+
+    public void setKullaniciAdi(String kullaniciAdi) {
+        this.kullaniciAdi = kullaniciAdi;
+    }
+
+    public String getParola() {
+        return parola;
+    }
+
+    public void setParola(String parola) {
+        this.parola = parola;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCepTel() {
+        return cepTel;
+    }
+
+    public void setCepTel(String cepTel) {
+        this.cepTel = cepTel;
+    }
+
+    public Integer getYas() {
+        return yas;
+    }
+
+    public void setYas(Integer yas) {
+        this.yas = yas;
+    }
+    
+
+        
+    
+    
+}
---
+++ src/main/java/tr/gov/ptt/gr1_simplereq/mb/UserController.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tr.gov.ptt.gr1_simplereq.mb;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import tr.gov.ptt.gr1_simplereq.dto.User;
+
+/**
+ *
+ * @author Administrator
+ */
+public class UserController {
+    
+    private User kullanici;
+
+    public User getKullanici() {
+        return kullanici;
+    }
+
+    public void setKullanici(User kullanici) {
+        this.kullanici = kullanici;
+    }
+
+    public UserController() {
+        this.kullanici = new User();
+    }
+    
+    public String kayit()
+    {
+       System.out.println("Kayıt edilen kullanıcı"+this.kullanici);
+       String msg = "Kullanıcı başarıyla kayıt edildi";
+       
+       //uyarı mesajıne ekranda gösterir
+       FacesContext.getCurrentInstance().addMessage(null, 
+                                          new FacesMessage(FacesMessage.SEVERITY_INFO, msg, msg));
+        
+        //Mesajın diger sayfada gözükmesini saglar.
+        FacesContext.getCurrentInstance().getExternalContext().getFlash().setKeepMessages(true);
+       
+      //?faces-redirect=true: gittigi sayfanın ismi yazsın browserda         
+       return "kayit.jsf?faces-redirect=true";
+    }
+    
+    
+}
